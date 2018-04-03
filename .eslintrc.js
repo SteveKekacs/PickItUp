7bec044f0e8782df176457af3ff1108ce347d8c9
tyrocca <tyler.rocca@gmail.com>
2018-04-03 11:57:35 -0400

    all these files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
 module.exports = {
-    "extends": "airbnb",
-    "globals": {
-      "window": true
-    }
+  "extends": "airbnb",
+  "globals": {
+    "window": true
+  },
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        "components": [ "Link" ],
+        "specialLink": [ "to", "hrefLeft", "hrefRight" ],
+        "aspects": [ "noHref", "invalidHref", "preferButton" ]
+      }
+    ]
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+/* eslint react/jsx-filename-extension: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 import registerServiceWorker from './registerServiceWorker';
+import App from './containers/app';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
