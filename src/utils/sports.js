ffdccc8670f2693b1e6b7cdc0874c65738b636e4
tyrocca <tyler.rocca@gmail.com>
2018-04-03 20:53:11 -0400

    made all files for nav

---
+++ src/utils/sports.js
@@ -0,0 +1,32 @@
+export const sportsList = [
+  "Baseball",
+  "Basketball",
+  "Boxing",
+  "Cycling",
+  "Darthmouth",
+  "Football",
+  "Golf",
+  "Hockey",
+  "Soccer",
+  "Swimming",
+  "Table Tennis",
+  "Tennis",
+  "Volleyball",
+];
+
+export const sportToFilter = {
+  Baseball: "baseball",
+  Basketball: "basketball",
+  Boxing: "boxing",
+  Cycling: "cycling",
+  Darthmouth: "darthmouth",
+  Football: "football",
+  Golf: "golf",
+  Hockey: "hockey",
+  Soccer: "soccer",
+  Swimming: "swimming",
+  "Table Tennis": "table_tennis",
+  Tennis: "tennis",
+  Volleyball: "volleyball",
+};
+
