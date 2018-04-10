5fae122fe42ecde49d5fb414a3dc171250d6351f
Skekacs93 <stevenkekacs@college.harvard.edu>
2018-04-10 11:18:57 -0400

    minor change

--- src/action-creators/filter-actions.js
+++ src/action-creators/filter-actions.js
@@ -1,7 +1,5 @@
 import * as types from '../action-types/FilterActions';
 
 export function filterActivities(sports, levels) {
-	console.log("FILTER THAT SHIT!!!!");
-	console.log(sports, levels);
-	return { type: types.FILTER_ACTIVITIES, sports, levels }
+    return { type: types.FILTER_ACTIVITIES, sports, levels }
 }
