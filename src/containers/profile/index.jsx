3b6cb94285966b8db5213cbf90b86b42ddcd2eb1
Skekacs93 <stevenkekacs@college.harvard.edu>
2018-04-10 14:27:15 -0400

    more work on profile

--- src/containers/profile/index.jsx
+++ src/containers/profile/index.jsx
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
+import Icon from 'material-ui/Icon';
+import IconButton from 'material-ui/IconButton';
+
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import * as actions from '../../action-creators/actions'
+import * as actions from '../../action-creators/actions';
 
 
 const styles = theme => ({
@@ -18,6 +21,32 @@ const styles = theme => ({
   subtext: {
     textAlign: 'center',
     color: theme.palette.text.primary
+  },
+  profile_icon: {
+    border: `2px solid ${theme.palette.text.secondary}`,
+    borderRadius: '100%',
+    width: '100px',
+    height: '100px',
+    fontSize: '100px'
+  },
+  icon: {
+    fontSize: '34px'
+  },
+  icon_div: {
+    textAlign: 'center',
+  },
+  icon_btn: {
+    border: `2px solid ${theme.palette.text.secondary}`,
+    width: '75px',
+    height: '75px'
+  },
+  icon_text: {
+    display: 'grid',
+    marginTop: '10px'
+  },
+  icon_seperator: {
+    display: 'inline-block',
+    width: '25px'
   }
 });
 
@@ -34,6 +63,7 @@ class Profile extends React.Component {
     return (
       <Grid item xs={12} >
         <Paper className={this.props.classes.paper}>
+          <Icon className={this.props.classes.profile_icon}>account_circle</Icon>
           <h1>{this.props.userInfo.first_name} {this.props.userInfo.last_name}</h1>
           <h2>{this.props.userInfo.age}</h2>
           <p>{this.props.userInfo.bio}</p>
@@ -43,6 +73,23 @@ class Profile extends React.Component {
               {this.props.userInfo.pastActivities.length} Activities
             </h2>)
         }
+        { parseInt(this.props.match.params.userId) !== this.props.currentUserId && 
+          <div className={this.props.classes.icon_div}>
+            <div style={{display: 'inline-block'}}>
+              <IconButton className={this.props.classes.icon_btn}>
+                <Icon className={this.props.classes.icon}>message</Icon>
+              </IconButton>
+              <span className={this.props.classes.icon_text}>Message</span>
+            </div>
+            <div className={this.props.classes.icon_seperator} />
+            <div style={{display: 'inline-block'}}>
+              <IconButton className={this.props.classes.icon_btn}>
+                <Icon className={this.props.classes.icon}>add</Icon>
+              </IconButton>
+              <span className={this.props.classes.icon_text}>Add Friend</span>
+            </div>
+          </div>
+        }
       </Grid>
   )}
 };
@@ -50,11 +97,13 @@ class Profile extends React.Component {
 Profile.propTypes = {
   classes: PropTypes.object.isRequired,
   userInfo: PropTypes.object,
+  currentUserId: PropTypes.number.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    userInfo: state.users.get('userInfo').toJS()
+    userInfo: state.users.get('userInfo').toJS(),
+    currentUserId: state.users.get('currentUserId')
   };
 };
 
